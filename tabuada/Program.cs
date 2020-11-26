45ea8be609eb24acd62b77db9d0ddde535f133de
yuri costa <yuri342costa@gmail.com>
2020-11-26 15:41:13 -0300

    c#

---
+++ tabuada/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            Console.WriteLine("voce quer ver a tabuada de qual número?");
+            int resposta = int.Parse(Console.ReadLine());
+
+            //int resultado;
+
+            for(int contador = 0; contador <= 10; contador ++){
+                //resultado multiplicando
+                Console.WriteLine($"{resposta} * {contador} = {resposta * contador}");
+                System.Threading.Thread.Sleep(1000);
+        }
+    }
+}
